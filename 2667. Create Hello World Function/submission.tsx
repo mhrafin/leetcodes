6e3a3afd71fba55ac70071b6d3e01bad7e22fb34
rafhome <110781737+mhrafin@users.noreply.github.com>
2026-04-10 15:05:09 +0600

    feat: add Create Hello World function solution

---
+++ 2667. Create Hello World Function/submission.tsx
@@ -0,0 +1,11 @@
+function createHelloWorld() {
+    
+    return function(...args:any): string {
+        return "Hello World"
+    };
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
